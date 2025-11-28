2fdd13404dc6d8c9b3bf802ae6f94955fd9675ed
Feraz ahmad <ferazahmadgit62@gmail.com>
2025-11-28 09:00:50 +0530

    completed login logic in frontend

--- Frontend/uibookmyshow/src/components/LoginSignup.tsx
+++ Frontend/uibookmyshow/src/components/LoginSignup.tsx
@@ -1,14 +1,26 @@
-import { useRef, useState } from "react";
-import axios from "axios";
-import Input from "./Input";
+import { useEffect, useRef, useState } from "react";
 
+import Input from "./Input";
+import { signupUser, loginUser } from "../slice/authSlice";
+
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { useNavigate } from "react-router-dom";
+import { type SignupProps } from "../types/types";
 const LoginSignup = () => {
   const [signup, setSignup] = useState(true);
   const emailRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const cnfPasswordRef = useRef<HTMLInputElement>(null);
+  const navigator = useNavigate();
+
+  const { token } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
 
   const signupHandler = () => {
+    if (!nameRef.current || nameRef.current.value.trim().length == 0) {
+      return alert("please fill name");
+    }
     if (!emailRef.current || emailRef.current.value.trim().length == 0) {
       return alert("please fill email or phone");
     }
@@ -22,12 +34,24 @@ const LoginSignup = () => {
       return alert("please fill confirm password or phone");
     }
     if (
-      passwordRef.current.value.trim().length ==
+      passwordRef.current.value.trim().length !==
       cnfPasswordRef.current.value.trim().length
     ) {
       return alert("your password and cnfirm password is not matching");
     }
+    const name = nameRef.current.value.trim();
+    const email = emailRef.current.value.trim();
+    const password = passwordRef.current.value.trim();
+    const confirmPassword = cnfPasswordRef.current.value.trim();
+    const data: SignupProps = { name, email, password, confirmPassword };
+    dispatch(signupUser(data));
   };
+
+  useEffect(() => {
+    if (token) {
+      navigator("/home");
+    }
+  }, [token]);
 
   const loginHandler = async () => {
     try {
@@ -40,21 +64,17 @@ const LoginSignup = () => {
       ) {
         return alert("please fill password or phone");
       }
-      const reqObj = {
-        identity: emailRef.current.value.trim(),
-        password: passwordRef.current.value,
-      };
-      const response = await axios.post(
-        "http://localhost:3000/api/v1/user",
-        reqObj
-      );
-      console.log(response);
+      const email = emailRef.current.value.trim();
+      const password = passwordRef.current.value.trim();
+      console.log("VVVVVVVVVVVVVVVVVVVV");
+      dispatch(loginUser({ email, password }));
     } catch (error) {
       console.log(error);
     }
   };
 
-  const formSubmitHandler = () => {
+  const formSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (signup) {
       signupHandler();
       return;
@@ -62,38 +82,42 @@ const LoginSignup = () => {
     loginHandler();
   };
   return (
-    <div className="py-10 px-4 shadow-2xl rounded-xl ">
-      <form
-        action=""
-        onSubmit={formSubmitHandler}
-        className="flex flex-col gap-2 justify-center w-[300px] "
-      >
-        <label className="m-0">Email or Phone</label>
-        <Input type="text" placeholder="Email or Phone" ref={emailRef} />
-        <label>Password</label>
-        <Input type="text" placeholder="password" ref={passwordRef} />
-        {signup && <label>Confirm Password</label>}
-        {signup && (
-          <Input
-            type="text"
-            placeholder="confirm password"
-            ref={cnfPasswordRef}
-          />
-        )}
-        <button className=" bg-[#0000ffbd] p-2 h-10 rounded-xl text-white">
-          Submit
-        </button>
-      </form>
-      <p>
-        <span>{signup ? "create a account" : "Login"} </span>
-        <span>or </span>
-        <span
-          className="text-blue-500 hover:border-b-2 "
-          onClick={() => setSignup((p) => !p)}
+    <div className="h-screen">
+      <div className="py-10 px-4 shadow-2xl rounded-xl ">
+        <form
+          action=""
+          onSubmit={formSubmitHandler}
+          className="flex flex-col gap-2 justify-center w-[300px] "
         >
-          {!signup ? "create a account" : "Login"}
-        </span>
-      </p>
+          {signup && <label>Name</label>}
+          {signup && <Input type="text" placeholder="Name" ref={nameRef} />}
+          <label>Email or Phone</label>
+          <Input type="text" placeholder="Email or Phone" ref={emailRef} />
+          <label>Password</label>
+          <Input type="text" placeholder="password" ref={passwordRef} />
+          {signup && <label>Confirm Password</label>}
+          {signup && (
+            <Input
+              type="text"
+              placeholder="confirm password"
+              ref={cnfPasswordRef}
+            />
+          )}
+          <button className=" bg-[#0000ffbd] p-2 h-10 rounded-xl text-white">
+            Submit
+          </button>
+        </form>
+        <p>
+          <span>{signup ? "create a account" : "Login"} </span>
+          <span>or </span>
+          <span
+            className="text-blue-500 hover:border-b-2 "
+            onClick={() => setSignup((p) => !p)}
+          >
+            {!signup ? "create a account" : "Login"}
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
---
+++ Frontend/uibookmyshow/src/types/types.ts
@@ -0,0 +1,11 @@
+export interface SignupProps {
+    name: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+}
+
+export interface LoginData {
+    email: string,
+    password: string
+}
